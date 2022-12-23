20ce0e35e7eeaa24a99b518880c7747eec8643d4
Gina Rodolfo <110985809+STINGRAY008@users.noreply.github.com>
2022-12-24 03:40:19 +0800

    New Updates

---
+++ admin/config/constants.php
@@ -0,0 +1,19 @@
+<?php
+    //Start Session
+    session_start();
+
+     //Create Constants to Store Non Repeating Values
+
+
+     define('SITEURL', 'http://localhost:8080/Superlove-Bistro-Order/');
+     define('LOCALHOST', 'localhost');
+     define('DB_USERNAME', 'root');
+     define('DB_PASSWORD', '');
+     define('DB_NAME', 'superlove-food-order');
+     
+    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
+    $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
+
+     //$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db); 
+  ?>
+
